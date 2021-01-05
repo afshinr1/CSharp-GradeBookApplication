6ed59ca00423740f2e4b78eaa86cdb8eff4f4995
Afshin <afshinrahman0@gmail.com>
2021-01-04 19:10:50 -0700

    add content

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,54 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            this.Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            return base.GetLetterGrade(averageGrade);
+        }
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires atleast {0} students", 5);
+                
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires atleast {0} students", 5);
+
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+
+
+
+    }
+}
